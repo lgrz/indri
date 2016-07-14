4b5cd655f6fa16191419f8e5cf2e5da1f8eae6b7
Luke Gallagher <luke@hypergeometric.net>
2016-07-14 22:08:56 +1000

    Revert `RMExpander` to version 2.1 (85e1a8)

--- src/RMExpander.cpp
+++ src/RMExpander.cpp
@@ -15,11 +15,10 @@
 // 18 Aug 2004 -- dam
 //
 
+#include "indri/RMExpander.hpp"
 #include <math.h>
 #include <stdio.h>
 #include <sstream>
-#include "indri/RMExpander.hpp"
-#include "indri/RelevanceModel.hpp"
 
 indri::query::RMExpander::RMExpander( indri::api::QueryEnvironment * env , indri::api::Parameters& param ) : indri::query::QueryExpander( env, param ) { }
 
@@ -29,46 +28,48 @@ std::string indri::query::RMExpander::expand( std::string originalQuery , std::v
   double fbOrigWt = _param.get( "fbOrigWeight", 0.5 );
   double mu = _param.get( "fbMu", 0 );
 
-  std::string rmSmoothing = "";
-  if (mu != 0) // specify dirichlet smoothing
-    rmSmoothing = "method:dirichlet,mu:" + (std::string)_param.get( "fbMu", "0");
+  std::vector<indri::api::DocumentVector*> docVectors = getDocumentVectors( results, fbDocs );
+  std::vector<std::string> * rm_vocab = getVocabulary( docVectors );
+  size_t vocabSize = rm_vocab->size();
+  UINT64 colLen = _env->termCount();
 
-  // this should be a parameter, have to change the generation to
-  // account for phrases then.
-  int maxGrams = 1;
-  
-  indri::query::RelevanceModel rm(*_env, rmSmoothing, maxGrams, fbDocs);
-  rm.generate( originalQuery, results );
-
-  const std::vector<indri::query::RelevanceModel::Gram*>& grams = rm.getGrams();
+  std::map<std::string, double> query_model;
   std::vector< std::pair<std::string, double> > probs;
-  // sorted grams came from rm
-  for( size_t j=0; j<grams.size(); j++ ) {
-    double w = grams[j]->weight;
-    // multi terms need to be handled if maxGrams becomes a parameter
-    std::string &term = grams[j]->terms[0]; 
-    probs.push_back( std::pair<std::string, double>( term, w ) );    
-  }
-  std::string expQuery;
-  
-  // if this was an extent restricted query, move the restriction outside
-  // the expansion.
-  // For field restrictions, it could be left nested, FixedPassageNode
-  // restrictions can not be nested in the query.
-  int openBrace = originalQuery.find('[');
-  if ( openBrace != std::string::npos) {
-    int closeBrace = originalQuery.find(']');
-    int firstParen = originalQuery.find('('); // must be one if '['
-    if ( openBrace < firstParen ) {
-      std::string qCopy = originalQuery;
-      // get the restriction
-      std::string restrict = originalQuery.substr(openBrace+1, 
-                                                  closeBrace-openBrace-1);
-      // remove the inner restriction
-      qCopy.erase(openBrace, closeBrace-openBrace+1);
-      expQuery = buildQuery( qCopy, fbOrigWt, probs, fbTerms );
-      return "#combine[" + restrict + "]( " + expQuery + " )";
+
+  // gather document vectors / statistics for top fbDocs ranked documents
+  for( int doc = 0; doc < fbDocs; doc++ ) {
+    if( doc >= results.size() ) break;
+    //std::cerr << "Expanding document " << doc << ", score = " << results[ doc ].score << std::endl;
+
+    indri::api::DocumentVector * docVec = docVectors[ doc ];
+    std::vector<int> term_positions = docVec->positions();
+    std::vector<std::string> term_list = docVec->stems();
+
+    size_t docLen = term_positions.size();
+
+    // add in "seen" proportion of term probability
+    std::vector<int>::iterator pos_iter;
+    for( pos_iter = term_positions.begin() ; pos_iter != term_positions.end() ; ++pos_iter )
+      query_model[ term_list[ *pos_iter ] ] += ( 1.0 / fbDocs ) * exp( results[ doc ].score ) / ( docLen + mu );
+
+    // add in "unseen" proportion of term probability
+    if( mu != 0.0 ) {
+      std::vector<std::string>::iterator vocab_iter;
+      for( vocab_iter = rm_vocab->begin() ; vocab_iter != rm_vocab->end() ; ++vocab_iter )
+        query_model[ *vocab_iter ] += ( 1.0 / fbDocs ) * exp( results[ doc ].score ) * ( mu * double( getCF( *vocab_iter ) )/double(colLen) ) / ( docLen + mu );
     }
+    
+    delete docVec;
   }
+
+  // sort terms based on P( w | \theta_Q )
+  std::vector<std::string>::iterator vocab_iter;
+  for( vocab_iter = rm_vocab->begin() ; vocab_iter != rm_vocab->end() ; ++vocab_iter )
+    probs.push_back( std::pair<std::string, double>( *vocab_iter , query_model[ *vocab_iter ] ) );    
+
+  std::sort( probs.begin() , probs.end() , QueryExpanderSort() );
+  delete rm_vocab;
+
   return buildQuery( originalQuery, fbOrigWt, probs, fbTerms );
 }
+
--- src/RelevanceModel.cpp
+++ src/RelevanceModel.cpp
@@ -81,33 +81,15 @@ void indri::query::RelevanceModel::_extractDocuments() {
 // gram occurs in each query result.
 //
 
-bool isValidWord(const string & word)
-{
-  size_t length = word.size();
-  const char * chArray = word.c_str();
-  size_t pos = 0;
-
-  while (pos < length)
-  { 
-    if(isalnum((unsigned char)*(chArray+pos)) == 0)
-    {
-      return false;
-    }
-    pos ++;
-  }
-  return true;
-}
-
 void indri::query::RelevanceModel::_countGrams() {
   // for each query result
-  for( size_t i=0; i<_results.size(); i++ ) {
+  for( int i=0; i<_results.size(); i++ ) {
     // run through the text, extracting n-grams
     indri::api::ScoredExtentResult& result = _results[i];
     indri::api::DocumentVector* v = _vectors[i];
     std::vector<int>& positions = v->positions();
     std::vector<std::string>& stems = v->stems();
-    if (result.end == 0) result.end = positions.size();
-    
+
     // for each word position in the text
     for( int j = result.begin; j < result.end; j++ ) {
       int maxGram = std::min( _maxGrams, result.end - j );
@@ -120,7 +102,7 @@ void indri::query::RelevanceModel::_countGrams() {
 
         // build the gram
         for( int k = 0; k < n; k++ ) {
-          if( positions[ k + j ] == 0 || (! isValidWord(stems[ positions[ k + j ] ])) ) {
+          if( positions[ k + j ] == 0 ) {
             containsOOV = true;
             break;
           }
@@ -163,7 +145,7 @@ void indri::query::RelevanceModel::_countGrams() {
 
 void indri::query::RelevanceModel::_scoreGrams() {
   HGram::iterator iter;
-  double collectionCount = (double)_environment.termCount();
+  double collectionCount = _environment.termCount();
   indri::query::TermScoreFunction* function = 0;  
 
   // for each gram we've seen
@@ -179,7 +161,7 @@ void indri::query::RelevanceModel::_scoreGrams() {
       // we're smoothing with them; otherwise we don't care.
 
       if( gram->terms.size() == 1 ) {
-        gramCount = (double)_environment.stemCount( gram->terms[0] );
+        gramCount = _environment.termCount( gram->terms[0] );
       } else {
         // notice that we're running a query here;
         // this is likely to be slow. (be warned)
@@ -187,7 +169,7 @@ void indri::query::RelevanceModel::_scoreGrams() {
         std::stringstream s;
         s << "#1( ";
 
-        for( size_t i=0; i< gram->terms.size(); i++ ) {
+        for( int i=0; i< gram->terms.size(); i++ ) {
           s << " \"" << gram->terms[i] << "\"" << std::endl;
         }
 
@@ -196,20 +178,18 @@ void indri::query::RelevanceModel::_scoreGrams() {
       }
 
       double gramFrequency = gramCount / collectionCount;
-      //      function = indri::query::TermScoreFunctionFactory::get( _smoothing, gramFrequency );
-      function = indri::query::TermScoreFunctionFactory::get( _smoothing, gramCount, collectionCount, 0 , 0 );
+      function = indri::query::TermScoreFunctionFactory::get( _smoothing, gramFrequency, collectionCount, 0, 0 );
     }
 
     // now, aggregate scores for each retrieved item
     std::vector<indri::api::ScoredExtentResult>::iterator riter;
     double gramScore = 0;
-    size_t c;
-    size_t r;
+    int c;
+    int r;
 
     for( r = 0, c = 0; r < _results.size() && c < gramCounts->counts.size(); r++ ) {
       int contextLength = _results[r].end - _results[r].begin;
-      // has been converted to a posterior probability.
-      double documentScore = _results[r].score;
+      double documentScore = exp( _results[r].score );
       double termScore = 0;
       double occurrences = 0;
 
@@ -221,15 +201,12 @@ void indri::query::RelevanceModel::_scoreGrams() {
 
       // determine the score for this term
       if( function != 0 ) {
-        // log probability here
-        termScore = exp(function->scoreOccurrence( occurrences, contextLength ));
+        termScore = function->scoreOccurrence( occurrences, contextLength );
       } else {
         termScore = occurrences / double(contextLength);
       }
-      //RMExpander weights this by 1/fbDocs
-      // Unclear as to why.
+
       gramScore += documentScore * termScore;
-      //gramScore += (1.0/_documents) * documentScore * termScore;
     }
 
     gram->weight = gramScore;
@@ -253,32 +230,6 @@ void indri::query::RelevanceModel::_sortGrams() {
   std::sort( _grams.begin(), _grams.end(), Gram::weight_greater() );
 }
 
-// In:  log(x1) log(x2) ... log(xN)
-// Out: x1/sum, x2/sum, ... xN/sum
-//
-// Extra care is taken to make sure we don't overflow
-// machine precision when taking exp (log x)
-// This is done by subtracting a constant K which cancels out
-// Right now K is set to maximally preserve the highest value
-// but could be altered to a min or average, or whatever...
-
-static void _logtoposterior(std::vector<indri::api::ScoredExtentResult> &res) {
-  if (res.size() == 0) return;
-  std::vector<indri::api::ScoredExtentResult>::iterator iter;
-  iter = res.begin();
-  double K = (*iter).score;
-  // first is max
-  double sum=0;
-
-  for (iter = res.begin(); iter != res.end(); iter++) {
-    sum += (*iter).score=exp((*iter).score - K);
-  }
-  for (iter = res.begin(); iter != res.end(); iter++) {
-    (*iter).score/=sum;
-  }
-}
-
-
 //
 // generate
 //
@@ -287,7 +238,6 @@ void indri::query::RelevanceModel::generate( const std::string& query ) {
   try {
     // run the query, get the document vectors
     _results = _environment.runQuery( query, _documents );
-    _logtoposterior(_results);
     _grams.clear();
     _extractDocuments();
     _vectors = _environment.documentVectors( _documentIDs );
@@ -295,30 +245,6 @@ void indri::query::RelevanceModel::generate( const std::string& query ) {
     _countGrams();
     _scoreGrams();
     _sortGrams();
-    for (unsigned int i = 0; i < _vectors.size(); i++)
-      delete _vectors[i];
-  } catch( lemur::api::Exception& e ) {
-    LEMUR_RETHROW( e, "Couldn't generate relevance model for '" + query + "' because: " );
-  }
-}
-
-//
-// generate
-//
-
-void indri::query::RelevanceModel::generate( const std::string& query, const std::vector<indri::api::ScoredExtentResult>& results  ) {
-  try {
-    _results = results;
-    _logtoposterior(_results);
-    _grams.clear();
-    _extractDocuments();
-    _vectors = _environment.documentVectors( _documentIDs );
-
-    _countGrams();
-    _scoreGrams();
-    _sortGrams();
-    for (unsigned int i = 0; i < _vectors.size(); i++)
-      delete _vectors[i];
   } catch( lemur::api::Exception& e ) {
     LEMUR_RETHROW( e, "Couldn't generate relevance model for '" + query + "' because: " );
   }
